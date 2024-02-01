a0efaf7ecba78a515582e948b93840d7cd628b55
Raghav Thapa <raghavthapa7@gmail.com>
2024-02-01 16:44:13 +0545

    db

--- src/services/db.js
+++ src/services/db.js
@@ -7,30 +7,28 @@ export const Stores = {
 };
 
 export const initDB = () => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    version++; 
+    request = indexedDB.open('myCategory', version); 
 
-    request = indexedDB.open('myCategory');
+    request.onupgradeneeded = (event) => {
+      db = event.target.result;
 
-    request.onupgradeneeded = () => {
-      db = request.result;
-
-    
       if (!db.objectStoreNames.contains(Stores.Categories)) {
         console.log('Creating categories store');
         db.createObjectStore(Stores.Categories, { keyPath: 'id' });
       }
-    
     };
 
     request.onsuccess = () => {
       db = request.result;
-      version = db.version;
-      console.log('request.onsuccess - initDB', version);
+      console.log('request.onsuccess - initDB', db.version);
       resolve(true);
     };
 
-    request.onerror = () => {
-      resolve(false);
+    request.onerror = (event) => {
+      console.log('request.onerror - initDB', event);
+      reject(event);
     };
   });
 };
@@ -38,4 +36,5 @@ export const initDB = () => {
 export const deleteDb = () => {
     indexedDB.deleteDatabase('myDBB')
 }
+
 
